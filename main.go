aad473b0b9b03b96538d1545501226d5cb7efe9c
Duan Rafael Ribeiro <duan.ribeiro@hotmail.com>
2020-02-20 16:29:45 -0300

    Minor, add some status code exceptions

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/gorilla/mux"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 
@@ -27,19 +30,20 @@ type Transfer struct {
 	CreatedAt string `json:"created_at"`
 }
 
-
+type allTranfers []Transfer
+var tranfers = allTranfers{}
 type allAccounts []Account
 var accounts = allAccounts{
 	{
 		ID: "1",
-		Name: "Roger Francisco",
+		Name: "Pedro Bala",
 		Cpf: "370.986.547-65",
 		Balance: 1000,
 		CreatedAt: "01-01-2020",
 	},
 	{
 		ID: "2",
-		Name: "Pedro Bala",
+		Name: "Capitu",
 		Cpf: "147.258.896-35",
 		Balance: 2000,
 		CreatedAt: "02-02-2020",
@@ -56,8 +60,32 @@ func getAccounts(w http.ResponseWriter, r *http.Request) {
 func createAccount(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var account Account
-	_ = json.NewDecoder(r.Body).Decode(&account)
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+
+	if err := json.Unmarshal(reqBody, &account); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422)
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+
+	json.Unmarshal(reqBody, &account)
+	json.NewDecoder(r.Body).Decode(&account)
+	currentTime := time.Now()
+
 	account.ID = strconv.Itoa(rand.Intn(1000000))
+	account.Balance = 0
+	account.CreatedAt = currentTime.Format("2006-01-02")
+
 	accounts = append(accounts, account)
 	json.NewEncoder(w).Encode(&account)
 }
@@ -79,8 +107,35 @@ func getAccountBalance(w http.ResponseWriter, r *http.Request) {
 func postTransfer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var transfer Transfer
-	reqBody, _ := ioutil.ReadAll(r.Body)
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+
+	if err := json.Unmarshal(reqBody, &transfer); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422)
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+
 	json.Unmarshal(reqBody, &transfer)
+	for _, account := range accounts {
+		if account.ID == transfer.AccountOriginId {
+			if account.Balance - transfer.Amount < 0 {
+				w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+				w.WriteHeader(400)
+				w.Write([]byte("AccountOriginId insufficient funds!"))
+				return
+			}
+		}
+	}
 
 	for _, account := range accounts {
 		if account.ID == transfer.AccountDestinationId {
@@ -89,7 +144,13 @@ func postTransfer(w http.ResponseWriter, r *http.Request) {
 			account.Balance = account.Balance - transfer.Amount
 		}
 	}
+	tranfers = append(tranfers, transfer)
 	json.NewEncoder(w).Encode(transfer)
+}
+
+func getTranfers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tranfers)
 }
 
 
@@ -99,8 +160,8 @@ func main() {
 	router.HandleFunc("/accounts", getAccounts).Methods("GET")
 	router.HandleFunc("/accounts/{id}/balance", getAccountBalance).Methods("GET")
 	router.HandleFunc("/accounts", createAccount).Methods("POST")
-
+	router.HandleFunc("/tranfers", getTranfers).Methods("GET")
 	router.HandleFunc("/tranfers", postTransfer).Methods("POST")
 
-	http.ListenAndServe(":8000", router)
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
